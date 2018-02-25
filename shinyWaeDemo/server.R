01a74d1066b4e704c976f5c0efc7a7dc51309e58
Steven H. Ranney <steven.ranney@gmail.com>
2018-02-25 14:06:51 -0700

    Just trying to update to ggplot. Shiny always makes me bang my head against my desk.

--- shinyWaeDemo/server.R
+++ shinyWaeDemo/server.R
@@ -1,4 +1,8 @@
 
+
+library(ggplot2)
+library(dplyr)
+
   wae <- read.table('data/WAE Clean.txt', header=T)
   
   shinyServer(function(input, output) {
@@ -7,69 +11,74 @@
       df <- wae[sample(nrow(wae), input$sampleSize, replace=TRUE), ]
       return(df)
     })
-    
 
-    #Create the linear least-squares regression model
-    lmMod <- reactive(function() {
-      lm(log10(dataset()$Weight)~log10(dataset()$Length))
+    #Create the least-squares regression model
+    lmMod <- reactive({
+      lm(log10(Weight)~log10(Length), data = dataset())
     })
     
-    #Create the linear quantile regression model
-    quantMod <- reactive(function(){
-      rq(log10(dataset()$Weight)~log10(dataset()$Length), tau=input$quantile)
+    #Create the quantile regression model
+    quantMod <- reactive({
+      rq(log10(Weight)~log10(Length), data = dataset(), tau=input$tau)
     })
     
-    #Create the nonlinear regression model
-    nlsMod <- reactive(function() {
-      nls(dataset()$Weight~alpha*dataset()$Length^beta,
-          start=list(alpha=0.000001, beta=3.0), data=dataset())
-    })
-    
-    #Create the nonlinear quantile regression model
-    nonlinQuantMod <- reactive(function(){
-      nlrq(dataset()$Weight~alpha*dataset()$Length^beta, 
-           start=list(alpha=0.000001, beta=3.0), tau=input$quantile)
-    })
-    
-    #Create a sequence of numbers from min-max Length
-    wae.mod <- reactive(function() {
-        seq(min(dataset()$Length), max(dataset()$Length), by=.1)
-    })
-    
-  
     output$plot <- renderPlot({
       
-      plot(Weight~Length, xlab = "Length (mm)", ylab = "Weight (g)", data=dataset())
-#        if (input$nlsRegression){
-#         lines(wae.mod(), predict(nlsMod(), list(Length=wae.mod())), col="red", lwd=2)
-#        }
-#        if (input$quantileRegression){
-#          lines(wae.mod(), predict(nonlinQuantMod(), list(Length=wae.mod())), col="blue", lwd=2)
-#        }
+      lin_plot <- 
+        ggplot(data = dataset(),
+               aes(x = log10(Length), y = log10(Weight))) +
+        geom_point(alpha = 0.5) +
+        labs(x = "Length (mm)", y = "Weight (g)")
       
-       
-    if (input$linear){
-      plot(log10(dataset()$Weight)~log10(dataset()$Length), 
-           xlab = "log10(Length)", ylab = "log10(Weight)")
-        if (input$lsRegression){
-          abline(lmMod(), col="red", lwd=2)
-          legend("topleft", paste0(round(summary(lmMod())$coef[1], 3), " + ", 
-                                    round(summary(lmMod())$coef[2], 3), " x log10(Length)"), 
-                 bty="n", lty=1, lwd=2, col="red")        
-        }
-        if (input$quantileRegression){
-          abline(quantMod(), col="blue", lwd=2, lty=2)
-          legend("bottomright", paste0(round(summary(quantMod())$coef[1], 3), " + ", 
-                                    round(summary(quantMod())$coef[2], 3), " x log10(Length)"), 
-                 bty="n", lty=2, lwd=2, col="blue")        
-        }
-    }
+      if(input$linear == FALSE){
+        ggplot(data = dataset(), 
+             aes(x = Length, y = Weight)) +
+        geom_point(alpha = 0.5) +
+        labs(x = "Length (mm)", y = "Weight (g)")
+      } else {
+
+        ggplot(data = dataset(),
+               aes(x = log10(Length), y = log10(Weight))) +
+          geom_point(alpha = 0.5) +
+          labs(x = "Length (mm)", y = "Weight (g)")
+        if(input$ols){
+            ggplot(data = dataset(),
+                   aes(x = log10(Length), y = log10(Weight))) +
+              geom_point(alpha = 0.5) +
+              labs(x = "Length (mm)", y = "Weight (g)") +
+              geom_abline(intercept = lmMod()$coefficients[1],
+                          slope = lmMod()$coefficients[2],
+                          colour = "red", lwd = 1)}
+          # if(input$qr){
+          #   ggplot(data = dataset(),
+          #          aes(x = log10(Length), y = log10(Weight))) +
+          #     geom_point(alpha = 0.5) +
+          #     labs(x = "Length (mm)", y = "Weight (g)") +
+          #     geom_abline(intercept = quantMod()$coefficients[1],
+          #                 slope = quantMod()$coefficients[2],
+          #                 colour = "blue", lwd = 1)}
+      }
+      
+      # if(input$linear){
+      #     if(input$ols){
+      #       ggplot(data = dataset(),
+      #              aes(x = log10(Length), y = log10(Weight))) +
+      #         geom_point(alpha = 0.5) +
+      #         labs(x = "Length (mm)", y = "Weight (g)") +
+      #         geom_abline(intercept = lmMod()$coefficients[1],
+      #                     slope = lmMod()$coefficients[2],
+      #                     colour = "red", lwd = 1)}
+      #       if(input$qr){
+      #         ggplot(data = dataset(),
+      #                aes(x = log10(Length), y = log10(Weight))) +
+      #           geom_point(alpha = 0.5) +
+      #           labs(x = "Length (mm)", y = "Weight (g)") +
+      #           geom_abline(intercept = quantMod()$coefficients[1],
+      #                       slope = quantMod()$coefficients[2],
+      #                       colour = "blue", lwd = 1)}
+      #   }
+          
         
-#    if (input$doICare){
-#      plot(dataset()$Length, dataset()$Weight, pch="", xlab = "Length (mm)", ylab = "Weight (g)")
-#      legend("center", "You don't care what happens here.", cex=1.5, bty="n")
-#    }
-    
       }) #Close renderPlot statement
     
     }) #Close shiny statement
--- shinyWaeDemo/ui.R
+++ shinyWaeDemo/ui.R
@@ -1,6 +1,8 @@
   
   library(shiny)
   library(quantreg)
+  library(ggplot2)
+  library(dplyr)
   
   wae <- read.table('data/WAE Clean.txt', header=T)
   
@@ -16,13 +18,12 @@
       sliderInput('sampleSize', 'Sample Size', min=50, max=nrow(wae),
                   value=min(1000, nrow(wae)), step=50, round=0),
     
-#      checkboxInput('nlsRegression', 'Display non-linear least-squares regression'),
       checkboxInput('linear', 'Linear'),
-      checkboxInput('lsRegression', 'Display linear least-squares regression'),
-      checkboxInput('quantileRegression', 'Display quantile regression'),
+      checkboxInput('ols', 'Display linear least-squares regression'),
+      checkboxInput('qr', 'Display quantile regression'),
       
-      sliderInput('quantile', 'Quantile', min=0.01, max=0.99, value=0.5, step=0.01),
-      em("Currently, quantile regression is implemented for linear data only.")
+      sliderInput('tau', 'Quantile', min=0.01, max=0.99, value=0.5, step=0.01),
+      em("Regression equations are implemented for linearized data only.")
 #      checkboxInput('doICare', "I don't care about modeling fisheries data.")
     
     ),
